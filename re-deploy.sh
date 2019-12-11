da9b6648e0ead7dfffe162f6c77dc8d48f5cf58b
Ingo Brunkhorst <Ingo.Brunkhorst@zuehlke.com>
2019-12-11 09:59:19 +0100

    Preparing v1.1.1 Release

--- re-deploy.sh
+++ re-deploy.sh
@@ -1,6 +1,12 @@
 #/bin/bash -f
 
 gradle app:build
+
 adb shell pm clear de.awi.floenavigation
-adb install -r app/build/outputs/apk/release/app-release.apk
+adb shell pm uninstall de.awi.floenavigation
+
+adb shell pm clear de.awi.floenavigation.debug
+adb shell pm uninstall de.awi.floenavigation.debug
+
+adb install app/build/outputs/apk/release/app-release.apk
 
