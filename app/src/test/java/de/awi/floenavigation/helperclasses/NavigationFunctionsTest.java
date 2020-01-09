d90074254ce114b53490580dc1bf22f0268a9b60
Ingo Brunkhorst <Ingo.Brunkhorst@zuehlke.com>
2020-01-09 14:58:46 +0100

    Test the NavigationFunctions

--- app/src/test/java/de/awi/floenavigation/helperclasses/NavigationFunctionsTest.java
+++ app/src/test/java/de/awi/floenavigation/helperclasses/NavigationFunctionsTest.java
@@ -13,13 +13,14 @@ public class NavigationFunctionsTest {
     @Test
     public void calculateDifference1() {
         double expected = 1851.31378905 * 60 * 10;
-        double actual = NavigationFunctions.calculateDifference(0,0,0,10);
+        double actual = NavigationFunctions.calculateDifference(0, 0, 0, 10);
         assertEquals(expected, actual, 0.0001);
     }
+
     @Test
     public void calculateDifference2() {
         double expected = 1851.31378905 * 60 * 10;
-        double actual = NavigationFunctions.calculateDifference(0,0,10,0);
+        double actual = NavigationFunctions.calculateDifference(0, 0, 10, 0);
         assertEquals(expected, actual, 0.0001);
     }
 
@@ -68,5 +69,33 @@ public class NavigationFunctionsTest {
 
     @Test
     public void locationInDegrees() {
+    }
+
+    @Test
+    public void calculateDifference() {
+    }
+
+    @Test
+    public void calculateNormalizedBearing() {
+    }
+
+    @Test
+    public void calculateBetaFromBearing() {
+        assertEquals(90.0, NavigationFunctions.calculateBetaFromBearing(0.0), 0.0001);
+        assertEquals(60.0, NavigationFunctions.calculateBetaFromBearing(30.0), 0.0001);
+        assertEquals(30.0, NavigationFunctions.calculateBetaFromBearing(60.0), 0.0001);
+        assertEquals(0.0, NavigationFunctions.calculateBetaFromBearing(90.0), 0.0001);
+        assertEquals(-30.0, NavigationFunctions.calculateBetaFromBearing(120.0), 0.0001);
+        assertEquals(-60.0, NavigationFunctions.calculateBetaFromBearing(150.0), 0.0001);
+        assertEquals(-90.0, NavigationFunctions.calculateBetaFromBearing(180.0), 0.0001);
+        assertEquals(-120.0, NavigationFunctions.calculateBetaFromBearing(210.0), 0.0001);
+        assertEquals(-150.0, NavigationFunctions.calculateBetaFromBearing(240.0), 0.0001);
+        assertEquals(180.0, NavigationFunctions.calculateBetaFromBearing(270.0), 0.0001);
+        assertEquals(150.0, NavigationFunctions.calculateBetaFromBearing(300.0), 0.0001);
+        assertEquals(120.0, NavigationFunctions.calculateBetaFromBearing(330.0), 0.0001);
+    }
+
+    @Test
+    public void calculateAngleBeta() {
     }
 }
